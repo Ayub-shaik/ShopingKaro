511293e462de6c08eac0426983fc5274a59241d5
ayubshaik <ayubshaik642@gmail.com>
2024-12-08 21:41:38 +0530

    modified index.js

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ const shopRoutes = require("./routes/shop");
 const PageNotFound = require("./helper/NotFound");
 const bodyParser = require("body-parser");
 const path = require("path");
-// const mongoConnect = require("./helper/database").mongoConnect;
 const User = require("./models/user");
 const mongoose = require("mongoose");
-const mongo_DB_URI = process.env.MONGO_DB_URI;
+
+// Define the MongoDB URI (update it based on your setup)
+const mongo_DB_URI = "mongodb://localhost:27017/mydatabase"; // Use this for local MongoDB
+// const mongo_DB_URI = "mongodb+srv://shaik:pblock@localhost:27017/mydatabase?retryWrites=true&w=majority"; // Use this for MongoDB Atlas
 
 const app = express();
 const PORT = 3000;
-// app.set("view engine", "pug");
+
+// Set view engine
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const rootDir = require("./helper/path");
+// Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Middleware to inject user
 app.use((req, res, next) => {
   User.findById("65aaa79a8945687d161ef472")
     .then((user) => {
@@ -31,22 +35,22 @@ app.use((req, res, next) => {
     });
 });
 
+// Routes
 app.use("/admin", adminRoutes);
-
 app.use(shopRoutes);
-
 app.use(PageNotFound);
 
+// Connect to MongoDB and start the server
 mongoose
-  .connect(mongo_DB_URI)
+  .connect(mongo_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
     app.listen(PORT, () => {
-      console.log("Database connected");
+      console.log("Database connected successfully");
       User.findOne().then((user) => {
         if (!user) {
           const user = new User({
-            name: "David",
-            email: "david@gmail.com",
+            name: "test-user",
+            email: "test-user@gmail.com",
             cart: {
               items: [],
             },
@@ -56,15 +60,9 @@ mongoose
           console.log("User Already Exist");
         }
       });
-      console.log("App is running on the port http://localhost:3000");
+      console.log(`App is running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
-    console.log(err);
+    console.log("Error connecting to the database:", err);
   });
-
-// mongoConnect(() => {
-//   app.listen(PORT, () => {
-//     console.log("App is running on the port http://localhost:3000");
-//   });
-// });
